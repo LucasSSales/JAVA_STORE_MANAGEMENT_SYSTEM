5b496029df0c11c8844ee62a84091422dc7effc1
LucasSSales <ldss@ic.ufal.br>
2017-09-17 23:19:45 -0300

    Add files via upload

--- Source Files/Strategy/Strategy.java
+++ Source Files/Strategy/Strategy.java
@@ -1,53 +1,82 @@
 package Strategy;
 
-import java.util.ArrayList;
+import java.awt.HeadlessException;
+import java.awt.Window;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-import allocation.Allocation;
-import users.User;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.ListModel;
+import javax.swing.WindowConstants;
 
-public class Strategy<T> {
+import Item_Menu.SellerItemManager;
+import MainProgram.SourceMain;
+import Signup_Login_Manager.LoginManager;
+
+public class Strategy {
 	
-	public ArrayList<T> add(ArrayList<T> a, T t){
-		if(a == null) 
-			a = new ArrayList<T>();
-		a.add(t);
-		return a;
-	}
-	
-	public Object search (ArrayList<T> a, String key) {
-		T found = null;
-		if(a!=null) {
-			for (T t : a) {
-				try {	
-					
-					Allocation x = (Allocation) t;
-					if(x.getName().equals(key) || x.getCode().equals(key) || x.getName().substring(0, 5).equals(key)) {
-						found = (T) x;
-						break;
-					}
-											
-				}catch(ClassCastException exception) {
-					
-					try {
-						
-						User x = (User) t;
-						if(x.getName().equals(key) || x.getID().equals(key)) {
-							found = (T) x;
-							break;
-						}
-						
-					}catch(ClassCastException excepition) {
-						
-						String s = (String) t;
-						if(s.equals(key)) {
-							found = (T) s;
-							break;
-						}
-						
-					}
-				}			
-			}
-		}
-		return found;
-	}	
+    public JFrame screen(JFrame j, LoginManager lmgr) {
+        j.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        j.setSize(800, 600);
+        j.setVisible(true);
+        lmgr.setVisible(false);
+    	return j;
+    }
+    
+    public JFrame searchUser(String table, String infos) throws HeadlessException, SQLException {
+    	ResultSet myRs = SourceMain.jDataBase.getQueryResult("SELECT * FROM " + table);
+    	String[] info = infos.split("/");
+        while (myRs.next()) {
+            if(info[0].equals(myRs.getString("name"))){
+            	if(info.length == 1) {
+            		return new JFrame();
+            	}else {
+                	if(info[1].equals(myRs.getString("password"))) {
+                		JOptionPane.showMessageDialog(null,"Login Successful");
+                		return new JFrame();
+                	}
+            	}
+            }
+        }
+    	return null;
+    }
+    
+    public void searchButton(String str, JList itemList, JLabel price, JLabel itemAvailable) {
+        ListModel listModel = itemList.getModel();
+        for (int i = 0; i < listModel.getSize(); i++) {
+            if (listModel.getElementAt(i).equals(str)) {
+
+                String[] serItem;
+                System.out.println(itemList.getSelectedValue());
+                ResultSet myRs = SourceMain.jDataBase.getQueryResult("SELECT * FROM `itemlist2` WHERE itemname REGEXP '" + str + "'");
+                try {
+                    myRs.next();
+                    price.setText("Price : " + myRs.getString("price"));
+                    itemAvailable.setText("Available : " + myRs.getString("quantity"));
+                    serItem = new String[]{myRs.getString("itemname")};
+
+                    price.setBounds(470, 50, price.getPreferredSize().width, price.getPreferredSize().height);
+                    itemAvailable.setBounds(470, 80, itemAvailable.getPreferredSize().width, itemAvailable.getPreferredSize().height);
+
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(null, "Database - Show Item Description Error");
+                    serItem = new String[]{"No Result"};
+                }
+                itemList.setListData(serItem);
+            }
+        }
+    }
+    
+    public void logoutButton(Window w, LoginManager lmg) {
+        String[] str = new String[]{"Yes","No"};
+        int logout = JOptionPane.showOptionDialog(null, "Are you sure you want to Log Out", "Logout", JOptionPane.NO_OPTION, JOptionPane.INFORMATION_MESSAGE, null, str, null);
+        if(logout == 0) {   //Delete Row From Table
+            w.dispose();
+            lmg.clearTextFields();
+            lmg.setVisible(true);
+        }
+    }
 }
